5728733d1f0d1c95df78126a895b08cd2b2baca0
NIHAL <nihal.gade@serole.com>
2024-08-28 12:31:29 +0530

    in local

--- src/main/java/com/example/nihalSpring/gitPratice/Feature2.java
+++ src/main/java/com/example/nihalSpring/gitPratice/Feature2.java
@@ -6,6 +6,6 @@ public class Feature2 {
 
     //creating merge confix now
     //in global commit changes 
-    //creating merge conflix in feature2class
+    //creating merge conflix in feature2class in local
 
 }
